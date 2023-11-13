23ab44ba4c7994a1fd9cc564fe3552e242be877b
donghyuk <donghyuk454@gmail.com>
2023-11-14 00:09:20 +0900

    [REFACTOR] builderFactory 사용하는 방식으로 변경

--- src/main/java/com/dream/application/batch/match/config/MatchBatchConfig.java
+++ src/main/java/com/dream/application/batch/match/config/MatchBatchConfig.java
@@ -4,24 +4,23 @@ import com.dream.application.batch.match.job.step.processor.MatchProcessor;
 import com.dream.application.batch.match.job.step.reader.MatchReader;
 import com.dream.application.batch.match.job.step.writer.MatchWriter;
 import com.dream.application.domain.match.entity.Match;
-
 import lombok.RequiredArgsConstructor;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class MatchBatchConfig {
 
-    private final JobRepository jobRepository;
-    private final PlatformTransactionManager transactionManager;
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
 
     private final MatchReader matchReader;
     private final MatchProcessor matchProcessor;
@@ -29,15 +28,19 @@ public class MatchBatchConfig {
 
     @Bean
     public Job matchJob(){
-        return new JobBuilder("matchJob", jobRepository)
+        log.info("job init");
+        return jobBuilderFactory.get("matchJob")
+                .preventRestart()
                 .start(matchStep())
                 .build();
     }
 
+    @JobScope
     @Bean
     public Step matchStep(){
-        return new StepBuilder("matchStep", jobRepository)
-                .<String, Match>chunk(10, transactionManager)
+        log.info("step init");
+        return stepBuilderFactory.get("matchStep")
+                .<String, Match>chunk(1)
                 .reader(matchReader)
                 .processor(matchProcessor)
                 .writer(matchWriter)
