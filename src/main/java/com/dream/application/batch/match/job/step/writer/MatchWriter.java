aa08d851cdf88ff4f9b934004cc8f223c18e2757
donghyuk <donghyuk454@gmail.com>
2023-11-16 00:50:04 +0900

    [REFACTOR] 스프링 버전 변경으로 override 함수 변경

--- src/main/java/com/dream/application/batch/match/job/step/writer/MatchWriter.java
+++ src/main/java/com/dream/application/batch/match/job/step/writer/MatchWriter.java
@@ -1,15 +1,18 @@
 package com.dream.application.batch.match.job.step.writer;
 
 import com.dream.application.domain.match.entity.Match;
-import org.springframework.batch.item.Chunk;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Slf4j
 @Component
 public class MatchWriter implements ItemWriter<Match> {
-
     @Override
-    public void write(Chunk<? extends Match> chunk) throws Exception {
-
+    public void write(List<? extends Match> items) {
+        log.info("size : " + items.size());
+        log.info("finished");
     }
 }
