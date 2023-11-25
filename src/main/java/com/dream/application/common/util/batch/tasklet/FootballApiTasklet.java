a98e982fa04ee3b6193e755153d65d383e966084
donghyuk <donghyuk454@gmail.com>
2023-11-25 19:39:01 +0900

    [FEAT] api 를 호출하여 item buffer 에 저장하는 로직 개발

--- src/main/java/com/dream/application/common/util/batch/tasklet/FootballApiTasklet.java
+++ src/main/java/com/dream/application/common/util/batch/tasklet/FootballApiTasklet.java
@@ -1,7 +1,10 @@
 package com.dream.application.common.util.batch.tasklet;
 
 import com.dream.application.common.util.batch.api.ItemBuffer;
+import com.dream.application.common.util.batch.dto.FootballApiBaseResponse;
 import com.dream.application.common.util.batch.dto.FootballApiInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Builder;
 import lombok.NonNull;
 import org.springframework.batch.core.StepContribution;
@@ -9,9 +12,9 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.constraints.NotBlank;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -24,7 +27,6 @@ public class FootballApiTasklet<T> implements Tasklet {
     private static final String BASE_URI = "https://api-football-v1.p.rapidapi.com/";
     private final FootballApiInfo footballApiInfo;
 
-    @Autowired
     private final ItemBuffer<T> responseBuffer;
 
     private final String endPoint;
@@ -40,7 +42,8 @@ public class FootballApiTasklet<T> implements Tasklet {
         this.responseBuffer = responseBuffer;
         this.endPoint = endPoint;
         this.method = method;
-        this.requestBody = Objects.requireNonNullElseGet(requestBody, HttpRequest.BodyPublishers::noBody);
+        this.requestBody = Objects.requireNonNullElseGet(requestBody,
+                HttpRequest.BodyPublishers::noBody);
         this.footballApiInfo = footballApiInfo;
     }
 
@@ -48,22 +51,48 @@ public class FootballApiTasklet<T> implements Tasklet {
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
         //HttpRequest.BodyPublishers.noBody()
         //"v3/fixtures/events?fixture=215662"
-        HttpRequest request = HttpRequest.newBuilder()
+        HttpRequest request = getRequest();
+
+        HttpResponse<String> response = sendGetRequest(request);
+
+        // error 검증
+        if (isInvalidResponse(response)) {
+            return RepeatStatus.FINISHED;
+        }
+
+        // 정상인 경우 확인
+        FootballApiBaseResponse<T> responseBody = getResponseBody(response);
+
+        if (responseBody.isError()) {
+            return RepeatStatus.FINISHED;
+        }
+
+        responseBuffer.addAll(responseBody.getResponse());
+
+        return RepeatStatus.FINISHED;
+    }
+
+    private FootballApiBaseResponse<T> getResponseBody(HttpResponse<String> response) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(response.body(), FootballApiBaseResponse.class);
+    }
+
+    private static boolean isInvalidResponse(HttpResponse<String> response) {
+        return response.statusCode() != 200;
+    }
+
+    private static HttpResponse<String> sendGetRequest(HttpRequest request) throws IOException, InterruptedException {
+        return HttpClient
+                .newHttpClient()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    private HttpRequest getRequest() {
+        return HttpRequest.newBuilder()
                 .uri(URI.create(BASE_URI + endPoint))
                 .header(footballApiInfo.getKeyNameKey(), footballApiInfo.getKeyNameValue())
                 .header(footballApiInfo.getHostNameKey(), footballApiInfo.getHostNameValue())
                 .method(method, requestBody)
                 .build();
-
-        HttpResponse<String> response = HttpClient
-                .newHttpClient()
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        // error 검증
-
-        // 정상인 경우 확인
-        response.body();
-
-        return null;
     }
 }
