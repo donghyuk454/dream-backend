91eb69f59e4b8378c913dccdf2392467893fc542
donghyuk <donghyuk454@gmail.com>
2023-12-29 17:53:29 +0900

    [FEAT] 제네릭의 타입을 가져올 수 없는 문제를 response DTO 의 타입을 생성자로 주입하는 방식으로 해결 및 기능 개발 마무리

--- src/main/java/com/dream/application/common/util/batch/api/tasklet/FootballApiTasklet.java
+++ src/main/java/com/dream/application/common/util/batch/api/tasklet/FootballApiTasklet.java
@@ -1,12 +1,14 @@
 package com.dream.application.common.util.batch.api.tasklet;
 
 import com.dream.application.common.util.batch.api.ItemBuffer;
-import com.dream.application.common.util.batch.dto.FootballApiBaseResponse;
-import com.dream.application.common.util.batch.dto.FootballApiInfo;
+import com.dream.application.common.util.batch.api.dto.FootballApiBaseResponse;
+import com.dream.application.common.util.batch.api.dto.FootballApiInfo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.*;
 import lombok.Builder;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
@@ -15,16 +17,19 @@ import org.springframework.batch.repeat.RepeatStatus;
 
 import javax.validation.constraints.NotBlank;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.Objects;
 
+@Slf4j
 @StepScope
-public class FootballApiTasklet<T> implements Tasklet {
+public class FootballApiTasklet<P, T> implements Tasklet {
 
-    private static final String BASE_URI = "https://api-football-v1.p.rapidapi.com/";
+    private static final String BASE_URI = "https://api-football-v1.p.rapidapi.com";
     private final FootballApiInfo footballApiInfo;
 
     private final ItemBuffer<T> responseBuffer;
@@ -32,25 +37,26 @@ public class FootballApiTasklet<T> implements Tasklet {
     private final String endPoint;
     private final String method;
     private final HttpRequest.BodyPublisher requestBody;
+    private final Type responseType;
 
     @Builder
     public FootballApiTasklet(@NonNull ItemBuffer<T> responseBuffer,
                               @NotBlank String endPoint,
                               @NotBlank String method,
                               HttpRequest.BodyPublisher requestBody,
-                              FootballApiInfo footballApiInfo) {
+                              FootballApiInfo footballApiInfo,
+                              Type responseType) {
         this.responseBuffer = responseBuffer;
         this.endPoint = endPoint;
         this.method = method;
         this.requestBody = Objects.requireNonNullElseGet(requestBody,
                 HttpRequest.BodyPublishers::noBody);
         this.footballApiInfo = footballApiInfo;
+        this.responseType = responseType;
     }
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        //HttpRequest.BodyPublishers.noBody()
-        //"v3/fixtures/events?fixture=215662"
         HttpRequest request = getRequest();
 
         HttpResponse<String> response = sendGetRequest(request);
@@ -61,18 +67,25 @@ public class FootballApiTasklet<T> implements Tasklet {
         }
 
         // 정상인 경우 확인
-        FootballApiBaseResponse<T> responseBody = getResponseBody(response);
+        List<T> responseList = getResponseBody(response).getResponse();
 
-        if (responseBody.isError()) {
-            return RepeatStatus.FINISHED;
-        }
-
-        responseBuffer.addAll(responseBody.getResponse());
+        // item buffer 에 item 추가
+        addToResponseBuffer(responseList);
 
         return RepeatStatus.FINISHED;
     }
 
-    private FootballApiBaseResponse<T> getResponseBody(HttpResponse<String> response) throws JsonProcessingException {
+    private void addToResponseBuffer(List<T> responseList) {
+        Gson gson = new GsonBuilder().create();
+
+        responseList.forEach(r -> {
+            String itemJson = gson.toJson(r);
+            T item = gson.fromJson(itemJson, responseType);
+            responseBuffer.add(item);
+        });
+    }
+
+    private FootballApiBaseResponse<P, T> getResponseBody(HttpResponse<String> response) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(response.body(), FootballApiBaseResponse.class);
     }
