6c2e2aef8e16fe497eb4ecc86dd34dfd17c0568e
donghyuk <donghyuk454@gmail.com>
2023-12-29 17:48:21 +0900

    [FEAT] league reader 개발

---
+++ src/main/java/com/dream/application/batch/league/job/step/reader/LeagueReader.java
@@ -0,0 +1,19 @@
+package com.dream.application.batch.league.job.step.reader;
+
+import com.dream.application.batch.league.job.dto.LeagueApiResponse;
+import com.dream.application.common.util.batch.api.ItemBuffer;
+import lombok.AllArgsConstructor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+@AllArgsConstructor
+public class LeagueReader implements ItemReader<LeagueApiResponse> {
+
+    private final ItemBuffer<LeagueApiResponse> itemBuffer;
+    @Override
+    public LeagueApiResponse read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+        return itemBuffer.poll();
+    }
+}
