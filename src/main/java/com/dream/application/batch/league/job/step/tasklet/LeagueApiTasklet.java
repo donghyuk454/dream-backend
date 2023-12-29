e7ce8f0da167c531616b27ac31f1246acc3308bd
donghyuk <donghyuk454@gmail.com>
2023-12-29 17:48:57 +0900

    [FEAT] league api 호출 배치 개발

---
+++ src/main/java/com/dream/application/batch/league/job/step/tasklet/LeagueApiTasklet.java
@@ -0,0 +1,20 @@
+package com.dream.application.batch.league.job.step.tasklet;
+
+import com.dream.application.batch.league.job.dto.LeagueApiResponse;
+import com.dream.application.batch.league.job.dto.SeasonParameter;
+import com.dream.application.common.util.batch.api.ItemBuffer;
+import com.dream.application.common.util.batch.api.dto.FootballApiInfo;
+import com.dream.application.common.util.batch.api.tasklet.FootballApiTasklet;
+import lombok.NonNull;
+
+public class LeagueApiTasklet extends FootballApiTasklet<SeasonParameter, LeagueApiResponse> {
+
+    private static final String SEASON = "2023";
+    private static final String ENDPOINT = "/v3/leagues?season=";
+    private static final String METHOD = "GET";
+
+    public LeagueApiTasklet(@NonNull ItemBuffer<LeagueApiResponse> responseBuffer,
+                            FootballApiInfo footballApiInfo) {
+        super(responseBuffer, ENDPOINT+SEASON , METHOD, null, footballApiInfo, LeagueApiResponse.class);
+    }
+}
