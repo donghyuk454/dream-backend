cf61da17a0f7c07e47b9cdaaa82e1e9c81769b74
donghyuk <donghyuk454@gmail.com>
2023-12-29 17:55:49 +0900

    [FEAT] 기본 BatchApplication 생성 (수정 필요)

---
+++ src/main/java/com/dream/application/BatchApplication.java
@@ -0,0 +1,18 @@
+package com.dream.application;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableBatchProcessing
+@EnableAutoConfiguration(exclude={BatchAutoConfiguration.class})
+@SpringBootApplication(exclude={BatchAutoConfiguration.class})
+@EnableJpaAuditing
+public class BatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
+}
