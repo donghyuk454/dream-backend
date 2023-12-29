927f39a6d6bc583988070dad6fbe4ae3528ce416
donghyuk <donghyuk454@gmail.com>
2023-12-29 17:50:53 +0900

    [TEST] football api 호출 시 필요한 parameter 를 제네릭 타입으로 변경 및 PagingDTO 추가 내용 테스트 수정

--- src/test/java/com/dream/application/common/util/batch/dto/FootballApiBaseResponseTest.java
+++ src/test/java/com/dream/application/common/util/batch/dto/FootballApiBaseResponseTest.java
@@ -1,5 +1,6 @@
 package com.dream.application.common.util.batch.dto;
 
+import com.dream.application.common.util.batch.api.dto.FootballApiBaseResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,7 @@ class FootballApiBaseResponseTest {
     @DisplayName("errors 가 비어 있으면 성공입니다.")
     void emptyErrorMeansSuccess() {
         //given
-        FootballApiBaseResponse response = new FootballApiBaseResponse(null,null, List.of(),null,null);
+        FootballApiBaseResponse response = new FootballApiBaseResponse(null,null,null,List.of(),null,null);
 
         //when
         boolean result = response.isError();
@@ -26,7 +27,7 @@ class FootballApiBaseResponseTest {
     @DisplayName("errors 가 비어 있지 않으면 실패입니다.")
     void notEmptyErrorMeansError() {
         //given
-        FootballApiBaseResponse response = new FootballApiBaseResponse(null,null, List.of(""),null,null);
+        FootballApiBaseResponse response = new FootballApiBaseResponse(null,null,null,List.of(""),null,null);
 
         //when
         boolean result = response.isError();
