b8cba5a07c96bf25f7fb360156609bf64e48f47d
donghyuk <donghyuk454@gmail.com>
2023-12-31 02:57:10 +0900

    [FEAT] football api 에서 제공하는 id 를 id로 사용하지 않고 별도의 column 으로 저장

---
+++ src/main/java/com/dream/application/common/entity/FootballEntity.java
@@ -0,0 +1,20 @@
+package com.dream.application.common.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class FootballEntity extends BaseEntity {
+    // football api 를 통해 얻은 id
+    // api 호출 시 해당 id 사용할 것
+    @Column(name = "FBA_ID")
+    protected Integer fbaId;
+}
