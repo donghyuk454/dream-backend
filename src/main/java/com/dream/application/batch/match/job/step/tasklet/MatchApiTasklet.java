bf8c97d33299be3fc4e4c82257374ac739f63f83
donghyuk <donghyuk454@gmail.com>
2024-01-01 17:37:22 +0900

    [FEAT] match api tasklet 개발

--- src/main/java/com/dream/application/batch/match/job/step/tasklet/MatchApiTasklet.java
+++ src/main/java/com/dream/application/batch/match/job/step/tasklet/MatchApiTasklet.java
@@ -1,27 +1,24 @@
 package com.dream.application.batch.match.job.step.tasklet;
 
+import com.dream.application.batch.match.job.dto.request.MatchParameter;
+import com.dream.application.batch.match.job.dto.response.MatchApiResponse;
 import com.dream.application.common.util.batch.api.ItemBuffer;
-import com.dream.application.batch.match.job.dto.MatchApiResponse;
-import lombok.RequiredArgsConstructor;
+import com.dream.application.common.util.batch.api.dto.FootballApiInfo;
+import com.dream.application.common.util.batch.api.tasklet.FootballApiTasklet;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
-public class MatchApiTasklet implements Tasklet {
+public class MatchApiTasklet extends FootballApiTasklet<MatchParameter, MatchApiResponse> {
 
-    private final ItemBuffer<MatchApiResponse> matchApiResponseItemBuffer;
+    private static final String SEASON = "2023";
+    private static final String ENDPOINT = "/v3/fixtures?season=" + SEASON + "&league=";
+    private static final String METHOD = "GET";
 
-    @Override
-    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
-        log.info("hihi");
-        log.info(contribution.toString());
-        log.info(chunkContext.toString());
-        return RepeatStatus.FINISHED;
+    public MatchApiTasklet(ItemBuffer<MatchApiResponse> responseBuffer,
+                           FootballApiInfo footballApiInfo,
+                           String league) {
+        super(responseBuffer, ENDPOINT+league , METHOD, null, footballApiInfo, MatchApiResponse.class);
     }
 }
