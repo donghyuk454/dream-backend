db14b6d1baac8dd241c1c087416816d162fe07b8
donghyuk <donghyuk454@gmail.com>
2024-01-01 17:39:55 +0900

    [FEAT] match reader 개발

--- src/main/java/com/dream/application/batch/match/job/step/reader/MatchReader.java
+++ src/main/java/com/dream/application/batch/match/job/step/reader/MatchReader.java
@@ -1,7 +1,7 @@
 package com.dream.application.batch.match.job.step.reader;
 
 import com.dream.application.common.util.batch.api.ItemBuffer;
-import com.dream.application.batch.match.job.dto.MatchApiResponse;
+import com.dream.application.batch.match.job.dto.response.MatchApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemReader;
@@ -16,9 +16,6 @@ public class MatchReader implements ItemReader<MatchApiResponse> {
 
     @Override
     public MatchApiResponse read() {
-        if(itemBuffer.isEmpty())
-            return null;
-
         return itemBuffer.poll();
     }
 }
