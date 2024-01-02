8cecbefb0b826ce08d06805dd5f47c29d0bb54ab
donghyuk <donghyuk454@gmail.com>
2024-01-03 01:41:55 +0900

    [REMOVE] 불필요 class 삭제

--- src/main/java/com/dream/application/batch/match/job/dto/MatchApiResponse.java
+++
@@ -1,7 +0,0 @@
-package com.dream.application.batch.match.job.dto;
-
-import lombok.Getter;
-
-@Getter
-public class MatchApiResponse {
-}
--- src/main/java/com/dream/application/domain/team/entity/League.java
+++
@@ -1,5 +0,0 @@
-package com.dream.application.domain.team.entity;
-
-public enum League { // unused
-    PREMIRE_LEAGUE, LA_LIGA, SERIE_A, BUNDESLIGA, LIGUE1
-}
