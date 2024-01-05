3a47f77a490061f5bf9b3fc5e2cb18e3bec84083
donghyuk <donghyuk454@gmail.com>
2024-01-05 17:47:46 +0900

    [FIX] league 주입 방식에서 조회 방식으로 변경 (entity detach)

--- src/main/java/com/dream/application/batch/team/job/step/writer/TeamWriter.java
+++ src/main/java/com/dream/application/batch/team/job/step/writer/TeamWriter.java
@@ -16,20 +16,14 @@ public class TeamWriter implements ItemWriter<Team> {
 
     private final TeamRepository teamRepository;
     private final TeamLeagueRepository teamLeagueRepository;
+    private final LeagueRepository leagueRepository;
 
-    private final League league;
-
-    public TeamWriter(LeagueRepository leagueRepository,
-                      TeamRepository teamRepository,
-                      TeamLeagueRepository teamLeagueRepository,
-                      Integer leagueFbaId) {
-        league = leagueRepository.findByFbaId(leagueFbaId);
-        this.teamRepository = teamRepository;
-        this.teamLeagueRepository = teamLeagueRepository;
-    }
+    private final Integer leagueFbaId;
 
     @Override
     public void write(List<? extends Team> items) {
+        League league = leagueRepository.findByFbaId(leagueFbaId);
+
         items.forEach(team -> {
             teamRepository.save(team);
             TeamLeague teamLeague = new TeamLeague(team, league);
