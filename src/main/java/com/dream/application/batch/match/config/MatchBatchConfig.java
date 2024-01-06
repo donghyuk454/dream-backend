d0380d2d55e6c153a5fbfa2f9fcf5409c5821b7b
donghyuk <donghyuk454@gmail.com>
2024-01-07 04:00:09 +0900

    [FEAT] MatchProcessorResponseDto 적용, transactionManager 추가, MatchProcessor/Writer 적용

--- src/main/java/com/dream/application/batch/match/config/MatchBatchConfig.java
+++ src/main/java/com/dream/application/batch/match/config/MatchBatchConfig.java
@@ -1,13 +1,17 @@
 package com.dream.application.batch.match.config;
 
-import com.dream.application.common.util.batch.api.ItemBuffer;
 import com.dream.application.batch.match.job.dto.response.MatchApiResponse;
+import com.dream.application.batch.match.job.step.dto.MatchProcessorResponseDto;
 import com.dream.application.batch.match.job.step.processor.MatchProcessor;
 import com.dream.application.batch.match.job.step.reader.MatchReader;
-import com.dream.application.batch.match.job.step.writer.MatchWriter;
 import com.dream.application.batch.match.job.step.tasklet.MatchApiTasklet;
+import com.dream.application.batch.match.job.step.writer.MatchWriter;
+import com.dream.application.common.util.batch.api.ItemBuffer;
 import com.dream.application.common.util.batch.api.dto.FootballApiInfo;
-import com.dream.application.domain.match.entity.Match;
+import com.dream.application.domain.league.repository.LeagueRepository;
+import com.dream.application.domain.match.repository.MatchRepository;
+import com.dream.application.domain.match.repository.TeamMatchRepository;
+import com.dream.application.domain.team.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -19,6 +23,10 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
 
 @Slf4j
 @Configuration
@@ -29,7 +37,19 @@ public class MatchBatchConfig {
     private final StepBuilderFactory stepBuilderFactory;
     private final FootballApiInfo footballApiInfo;
 
-    private static final String LEAGUE_ID = "342";
+    private final LeagueRepository leagueRepository;
+    private final TeamRepository teamRepository;
+    private final MatchRepository matchRepository;
+    private final TeamMatchRepository teamMatchRepository;
+
+    private final EntityManagerFactory entityManagerFactory;
+
+    private static final String LEAGUE_ID = "39";
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 
     @Bean
     public ItemBuffer<MatchApiResponse> matchApiResponseItemBuffer() {
@@ -38,7 +58,7 @@ public class MatchBatchConfig {
 
     @Bean
     @Qualifier("matchApiJob")
-    public Job matchJob(){
+    public Job matchApiJob(){
         return jobBuilderFactory.get("matchJob")
                 .preventRestart()
                 .start(callMatchApiStep())
@@ -58,10 +78,11 @@ public class MatchBatchConfig {
     @Bean
     public Step matchInitStep(){
         return stepBuilderFactory.get("matchStep")
-                .<MatchApiResponse, Match>chunk(1)
+                .<MatchApiResponse, MatchProcessorResponseDto>chunk(1)
                 .reader(matchReader())
                 .processor(matchProcessor())
                 .writer(matchWriter())
+                .transactionManager(transactionManager(entityManagerFactory))
                 .build();
     }
 
@@ -80,12 +101,12 @@ public class MatchBatchConfig {
     @StepScope
     @Bean
     public MatchProcessor matchProcessor() {
-        return new MatchProcessor();
+        return new MatchProcessor(leagueRepository, teamRepository);
     }
 
     @StepScope
     @Bean
     public MatchWriter matchWriter() {
-        return new MatchWriter();
+        return new MatchWriter(matchRepository, teamMatchRepository);
     }
 }
