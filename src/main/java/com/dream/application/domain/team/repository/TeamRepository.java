00d79cd3703e81625ae4a8c5bd969711fafc830b
donghyuk <donghyuk454@gmail.com>
2024-01-07 03:01:25 +0900

    [FEAT] findByFbaId 개발

--- src/main/java/com/dream/application/domain/team/repository/TeamRepository.java
+++ src/main/java/com/dream/application/domain/team/repository/TeamRepository.java
@@ -4,4 +4,5 @@ import com.dream.application.domain.team.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    Team findByFbaId(Integer fbaId);
 }
