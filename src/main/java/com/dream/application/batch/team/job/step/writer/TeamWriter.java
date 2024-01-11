d11a6803ba62bc555fcaf137860678e83f5a30ff
donghyuk <donghyuk454@gmail.com>
2024-01-11 21:55:42 +0900

    [FEAT] TeamWriter 로직 고도화 - 존재하지 않는 경우 추가

--- src/main/java/com/dream/application/batch/team/job/step/writer/TeamWriter.java
+++ src/main/java/com/dream/application/batch/team/job/step/writer/TeamWriter.java
@@ -7,10 +7,12 @@ import com.dream.application.domain.team.entity.TeamLeague;
 import com.dream.application.domain.team.repository.TeamLeagueRepository;
 import com.dream.application.domain.team.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemWriter;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 public class TeamWriter implements ItemWriter<Team> {
 
@@ -21,13 +23,37 @@ public class TeamWriter implements ItemWriter<Team> {
     private final Integer leagueFbaId;
 
     @Override
-    public void write(List<? extends Team> items) {
-        League league = leagueRepository.findByFbaId(leagueFbaId);
+    public void write(List<? extends Team> items) throws Exception{
+        League league = leagueRepository.findByFbaId(leagueFbaId)
+                .orElseThrow(() -> new Exception("존재하지 않는 LEAGUE 입니다."));
 
         items.forEach(team -> {
-            teamRepository.save(team);
+            saveTeamIfNotPresent(team);
+
             TeamLeague teamLeague = new TeamLeague(team, league);
-            teamLeagueRepository.save(teamLeague);
+
+            saveTeamLeagueIfNotPresent(teamLeague);
         });
+    }
+
+    private void saveTeamLeagueIfNotPresent(TeamLeague teamLeague) {
+        boolean isPresent = teamLeagueRepository.existsByTeamAndLeague(teamLeague.getTeam(), teamLeague.getLeague());
+
+        if (isPresent) return;
+
+        teamLeagueRepository.save(teamLeague);
+
+        log.info("신규 생성된 TEAM_LEAGUE 입니다. TEAM_ID : {}, LEAGUE_ID : {}",
+                teamLeague.getTeam().getTeamId(),
+                teamLeague.getLeague().getLeagueId());
+    }
+
+    private void saveTeamIfNotPresent(Team team) {
+        boolean isPresent = teamRepository.existsByFbaId(team.getFbaId());
+
+        if(isPresent) return;
+
+        teamRepository.save(team);
+        log.info("신규 생성된 TEAM 입니다. FBA_ID : {}", team.getFbaId());
     }
 }
