9a052955038c158cc7bd0419c99d24e412ca890b
donghyuk <donghyuk454@gmail.com>
2024-01-11 21:50:46 +0900

    [FEAT] MatchWriter 로직 고도화 - 존재하지 않는 경우 추가 - 존재하는 경우 sync

--- src/main/java/com/dream/application/batch/match/job/step/writer/MatchWriter.java
+++ src/main/java/com/dream/application/batch/match/job/step/writer/MatchWriter.java
@@ -5,6 +5,7 @@ import com.dream.application.domain.match.entity.Match;
 import com.dream.application.domain.match.entity.TeamMatch;
 import com.dream.application.domain.match.repository.MatchRepository;
 import com.dream.application.domain.match.repository.TeamMatchRepository;
+import com.dream.application.domain.team.entity.Team;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemWriter;
@@ -25,14 +26,29 @@ public class MatchWriter implements ItemWriter<MatchProcessorResponseDto> {
     public void write(List<? extends MatchProcessorResponseDto> items) {
         items.forEach(item -> {
             // match 저장
-            Match match = matchRepository.save(item.getMatch());
+            Match match = item.getMatch();
+            matchRepository.findByFbaId(match.getFbaId())
+                    .ifPresentOrElse(
+                            m -> m.syncMatch(match), // 존재할 경우 Sync
+                            () -> matchRepository.save(match) // 존재하지 않을 경우 저장
+                    );
 
             // team match 저장
             Set<TeamMatch> teamMatches = item.getTeamMatches();
-            teamMatches.forEach(teamMatch -> {
-                teamMatch.setMatch(match);
-                teamMatchRepository.save(teamMatch);
-            });
+            teamMatches.forEach(teamMatch -> addTeamMatchIfNotPresent(match, teamMatch));
         });
+    }
+
+    private void addTeamMatchIfNotPresent(Match match, TeamMatch teamMatch) {
+        Team team = teamMatch.getTeam();
+        boolean isPresent = teamMatchRepository.existsByTeamAndMatch(team, match);
+
+        if (!isPresent) {
+            teamMatch.setMatch(match);
+            teamMatchRepository.save(teamMatch);
+            log.info("TEAM_MATCH 추가. TEAM_ID : {}, MATCH_ID : {}",
+                    team.getTeamId(),
+                    match.getMatchId());
+        }
     }
 }
