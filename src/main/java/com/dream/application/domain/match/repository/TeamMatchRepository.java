785d835df2a1c406ec2d8d5b2d0a3253047726f5
donghyuk <donghyuk454@gmail.com>
2024-01-11 21:36:54 +0900

    [FEAT] existsByTeamAndMatch 개발

--- src/main/java/com/dream/application/domain/match/repository/TeamMatchRepository.java
+++ src/main/java/com/dream/application/domain/match/repository/TeamMatchRepository.java
@@ -1,7 +1,10 @@
 package com.dream.application.domain.match.repository;
 
+import com.dream.application.domain.match.entity.Match;
 import com.dream.application.domain.match.entity.TeamMatch;
+import com.dream.application.domain.team.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamMatchRepository extends JpaRepository<TeamMatch, Long> {
+    boolean existsByTeamAndMatch(Team team, Match match); // 성능 개선 필요
 }
