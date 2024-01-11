7d88053152a3597505c21de81b1c69c4d65d3325
donghyuk <donghyuk454@gmail.com>
2024-01-11 21:37:49 +0900

    [FEAT] existsByTeamAndLeague 개발

--- src/main/java/com/dream/application/domain/team/repository/TeamLeagueRepository.java
+++ src/main/java/com/dream/application/domain/team/repository/TeamLeagueRepository.java
@@ -1,7 +1,10 @@
 package com.dream.application.domain.team.repository;
 
+import com.dream.application.domain.league.entity.League;
+import com.dream.application.domain.team.entity.Team;
 import com.dream.application.domain.team.entity.TeamLeague;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamLeagueRepository extends JpaRepository<TeamLeague, Long> {
+    boolean existsByTeamAndLeague(Team team, League league);
 }
