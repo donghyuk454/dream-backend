7c7f0e138906c1ae8d741b1249dc32cf2bfbdf6c
donghyuk <donghyuk454@gmail.com>
2024-01-18 00:39:32 +0900

    [FEAT] Session key 를 상수 처리

---
+++ src/main/java/com/dream/application/common/session/SessionConst.java
@@ -0,0 +1,9 @@
+package com.dream.application.common.session;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SessionConst {
+    public static final String LOGIN_SESSION_KEY = "loginMember";
+}
