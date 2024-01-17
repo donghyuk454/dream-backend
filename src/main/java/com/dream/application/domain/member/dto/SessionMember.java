c5c92eef56258042cfd580862fc337866e5de0a9
donghyuk <donghyuk454@gmail.com>
2024-01-18 00:38:41 +0900

    [FEAT] member id type 변경

--- src/main/java/com/dream/application/domain/member/dto/SessionMember.java
+++ src/main/java/com/dream/application/domain/member/dto/SessionMember.java
@@ -1,14 +1,16 @@
 package com.dream.application.domain.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Getter
+@AllArgsConstructor
 public class SessionMember implements Serializable {
 
-    private String memberId;
+    private Long memberId;
     private String username;
 
     @Serial
