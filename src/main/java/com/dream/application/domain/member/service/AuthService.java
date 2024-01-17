7edf7878cc377dc8030a8f542da6d0c23d8a1d34
donghyuk <donghyuk454@gmail.com>
2024-01-17 23:02:05 +0900

    [FEAT] AuthService 개발

---
+++ src/main/java/com/dream/application/domain/member/service/AuthService.java
@@ -0,0 +1,23 @@
+package com.dream.application.domain.member.service;
+
+import com.dream.application.common.exception.DreamException;
+import com.dream.application.domain.member.entity.Member;
+import com.dream.application.domain.member.repository.AuthMemberRepository;
+import com.dream.application.domain.member.service.dto.request.LoginServiceRequest;
+import com.dream.application.domain.member.service.dto.response.LoginServiceResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthMemberRepository authMemberRepository;
+
+    public LoginServiceResponse login(LoginServiceRequest loginRequest) {
+        Member member = authMemberRepository.findById(loginRequest.getMemberId())
+                .orElseThrow(() -> new DreamException("member.notfound.error"));
+
+        return LoginServiceResponse.of(member);
+    }
+}
