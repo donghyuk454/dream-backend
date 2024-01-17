49831c24e6cf76a13425b564be3e4fb6d05c4d44
donghyuk <donghyuk454@gmail.com>
2024-01-18 00:35:35 +0900

    [STYLE] 불필요 매서드 제거

--- src/main/java/com/dream/application/domain/member/controller/dto/response/LoginResponse.java
+++ src/main/java/com/dream/application/domain/member/controller/dto/response/LoginResponse.java
@@ -1,7 +1,6 @@
 package com.dream.application.domain.member.controller.dto.response;
 
 import com.dream.application.domain.member.dto.SessionMember;
-import com.dream.application.domain.member.service.dto.response.LoginServiceResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,8 +8,4 @@ import lombok.Getter;
 @AllArgsConstructor
 public class LoginResponse {
     private SessionMember sessionMember;
-
-    public static LoginResponse of(LoginServiceResponse loginServiceResponse) {
-        return new LoginResponse(loginServiceResponse.getSessionMember());
-    }
 }
