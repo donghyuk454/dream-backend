c3b04818a8c28c4e1dc832be033f5a68787d5ccc
donghyuk <donghyuk454@gmail.com>
2024-01-18 21:43:05 +0900

    [FEAT] ExceptionHandler 개발

---
+++ src/main/java/com/dream/application/web/exception/ExceptionHandlers.java
@@ -0,0 +1,20 @@
+package com.dream.application.web.exception;
+
+import com.dream.application.common.exception.DreamException;
+import com.dream.application.common.response.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionHandlers {
+
+    @ResponseBody
+    @ExceptionHandler(DreamException.class)
+    public BaseResponse<String> handleException(Exception e){
+        log.info("에러 발생 message = {}", e.getMessage());
+        return BaseResponse.error(e.getMessage());
+    }
+}
