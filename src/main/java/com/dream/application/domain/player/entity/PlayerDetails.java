0bc97169f72d40be7ba0d423a4562cb57d19aca9
donghyuk <donghyuk454@gmail.com>
2024-01-25 01:17:13 +0900

    [FEAT] Getter 추가

--- src/main/java/com/dream/application/domain/player/entity/PlayerDetails.java
+++ src/main/java/com/dream/application/domain/player/entity/PlayerDetails.java
@@ -1,11 +1,13 @@
 package com.dream.application.domain.player.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+@Getter
 @Embeddable
 @AllArgsConstructor
 @NoArgsConstructor
