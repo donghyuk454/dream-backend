7affe422f9337a3c2ca0cb6520b999268993cfaa
donghyuk <donghyuk454@gmail.com>
2024-01-25 01:15:39 +0900

    [FEAT] id 변경

--- src/main/java/com/dream/application/domain/subscribe/entity/MemberSubscription.java
+++ src/main/java/com/dream/application/domain/subscribe/entity/MemberSubscription.java
@@ -2,6 +2,7 @@ package com.dream.application.domain.subscribe.entity;
 
 import com.dream.application.common.entity.BaseEntity;
 import com.dream.application.domain.member.entity.Member;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,16 +13,22 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 public class MemberSubscription extends BaseEntity {
-    @EmbeddedId
-    private MemberSubscriptionId id;
 
-    @MapsId(value = "memberId")
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_SUBSCRIPTION_ID")
+    private Long memberSubscriptionId;
+
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
-    @MapsId(value = "subscriptionId")
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "SUBSCRIPTION_ID")
     private Subscription subscription;
+
+    @Builder
+    public MemberSubscription(Member member, Subscription subscription) {
+        this.member = member;
+        this.subscription = subscription;
+    }
 }
