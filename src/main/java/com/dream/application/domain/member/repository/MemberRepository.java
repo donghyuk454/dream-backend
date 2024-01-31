c9242d4ab895ae246b8aeec394b67b78e0c212c2
donghyuk <donghyuk454@gmail.com>
2024-02-01 00:47:22 +0900

    [FEAT] Fetch Join 을 통한 조회 기능 개발

--- src/main/java/com/dream/application/domain/member/repository/MemberRepository.java
+++ src/main/java/com/dream/application/domain/member/repository/MemberRepository.java
@@ -2,6 +2,17 @@ package com.dream.application.domain.member.repository;
 
 import com.dream.application.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("SELECT m FROM Member m " +
+            "LEFT JOIN FETCH m.memberSubscriptions ms " +
+            "LEFT JOIN FETCH ms.subscription s " +
+            "LEFT JOIN FETCH s.player " +
+            "WHERE m.memberId = :memberId")
+    Optional<Member> findWithMemberSubscriptionsById(@Param("memberId") Long memberId);
 }
