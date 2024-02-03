e4acc3b98af700174b3c9f5f48878f6ee6094672
donghyuk <donghyuk454@gmail.com>
2024-02-03 17:14:13 +0900

    [FEAT] 요구사항 변경으로 인한 Dto 객체 변경 AS-IS : playerName 만 return TO-BE : playerId + playerName

--- src/main/java/com/dream/application/domain/member/controller/dto/response/FindMemberSubscriptionResponse.java
+++ src/main/java/com/dream/application/domain/member/controller/dto/response/FindMemberSubscriptionResponse.java
@@ -1,10 +1,16 @@
 package com.dream.application.domain.member.controller.dto.response;
 
+import com.dream.application.domain.member.service.dto.response.PlayerInfoDto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public class FindMemberSubscriptionResponse {
+    private final Long playerId;
     private final String playerName;
+
+    public static FindMemberSubscriptionResponse of (PlayerInfoDto playerInfo) {
+        return new FindMemberSubscriptionResponse(playerInfo.getPlayerId(), playerInfo.getPlayerName());
+    }
 }
