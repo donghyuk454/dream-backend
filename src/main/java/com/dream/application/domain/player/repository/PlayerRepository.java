828f283ef13d5ba80d157f52185586f8f1eb45a2
donghyuk <donghyuk454@gmail.com>
2024-02-06 21:56:53 +0900

    [FEAT] fetch join 을 사용해 조회 기능 개발

--- src/main/java/com/dream/application/domain/player/repository/PlayerRepository.java
+++ src/main/java/com/dream/application/domain/player/repository/PlayerRepository.java
@@ -2,6 +2,14 @@ package com.dream.application.domain.player.repository;
 
 import com.dream.application.domain.player.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+    @Query("SELECT p FROM Player p " +
+            "LEFT JOIN FETCH p.team t " +
+            "WHERE p.playerId = :playerId")
+    Optional<Player> findWithTeamById(@Param("playerId") Long playerId);
 }
