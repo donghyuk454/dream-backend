981e5e7e10be1df56388567f4d167b61c3b74a24
donghyuk <donghyuk454@gmail.com>
2024-02-13 22:39:16 +0900

    [FEAT] findAllByTeam 개발

--- src/main/java/com/dream/application/domain/match/repository/TeamMatchRepository.java
+++ src/main/java/com/dream/application/domain/match/repository/TeamMatchRepository.java
@@ -5,6 +5,10 @@ import com.dream.application.domain.match.entity.TeamMatch;
 import com.dream.application.domain.team.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TeamMatchRepository extends JpaRepository<TeamMatch, Long> {
     boolean existsByTeamAndMatch(Team team, Match match); // 성능 개선 필요
+
+    List<TeamMatch> findAllByTeam(Team team); // 성능 개선 필요
 }
