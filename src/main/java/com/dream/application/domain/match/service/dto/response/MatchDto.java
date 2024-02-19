4ed135b4f7e1c560a5651867ed995d06e23988c5
donghyuk <donghyuk454@gmail.com>
2024-02-19 22:48:46 +0900

    [FEAT] json format 추가

--- src/main/java/com/dream/application/domain/match/service/dto/response/MatchDto.java
+++ src/main/java/com/dream/application/domain/match/service/dto/response/MatchDto.java
@@ -1,6 +1,7 @@
 package com.dream.application.domain.match.service.dto.response;
 
 import com.dream.application.domain.match.entity.Match;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +14,7 @@ public class MatchDto {
     private final String league;
     private final String home;
     private final String away;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime schedule;
 
     public MatchDto(Match match) {
