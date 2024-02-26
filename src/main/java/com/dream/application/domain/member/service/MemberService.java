e8ec2aa362bdd8db5204a92697bab2a4e8591bfd
donghyuk <donghyuk454@gmail.com>
2024-02-26 22:47:35 +0900

    [FEAT] 회원 가입 기능 개발

--- src/main/java/com/dream/application/domain/member/service/MemberService.java
+++ src/main/java/com/dream/application/domain/member/service/MemberService.java
@@ -3,7 +3,9 @@ package com.dream.application.domain.member.service;
 import com.dream.application.common.exception.DreamException;
 import com.dream.application.domain.member.entity.Member;
 import com.dream.application.domain.member.repository.MemberRepository;
+import com.dream.application.domain.member.service.dto.request.AddMemberServiceRequest;
 import com.dream.application.domain.member.service.dto.request.FindMemberServiceRequest;
+import com.dream.application.domain.member.service.dto.response.AddMemberServiceResponse;
 import com.dream.application.domain.member.service.dto.response.FindMemberServiceResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,5 +26,12 @@ public class MemberService {
                 .orElseThrow(() -> new DreamException("member.notfound.error"));
 
         return FindMemberServiceResponse.of(member);
+    }
+
+    public AddMemberServiceResponse addMember(AddMemberServiceRequest request) {
+        Member member = new Member(request.getEmail(), request.getName());
+        memberRepository.save(member);
+
+        return new AddMemberServiceResponse(member.getMemberId());
     }
 }
