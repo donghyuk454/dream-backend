d71020c488a1e2242e2befd6c2ad1190e71af00b
donghyuk <donghyuk454@gmail.com>
2024-03-25 23:11:44 +0900

    [FIX] 기본 생성자 생성

--- src/main/java/com/dream/application/domain/member/controller/dto/request/LoginRequest.java
+++ src/main/java/com/dream/application/domain/member/controller/dto/request/LoginRequest.java
@@ -2,9 +2,11 @@ package com.dream.application.domain.member.controller.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     private Long memberId;
 }
